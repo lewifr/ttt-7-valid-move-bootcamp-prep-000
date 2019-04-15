3e9fde2217f9b58e6d4c0962dae60aff9823240c
Francis Lewis <francis.lewis@me.com>
2019-04-15 14:23:18 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,11 @@
+def position_taken?(board, index)
+  board[index] != " " && board[index] != "" && board[index] != nil
+end
+
 def valid_move?(board, index)
   if (index.between?(0,9) && !position_taken(board, index))
     true
   else
     false
   end
-end
-
-def position_taken?(board, index)
-  board[index] != " " && board[index] != "" && board[index] != nil
 end
